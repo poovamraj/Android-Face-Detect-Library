c63982c375c62ced8e4897f2de0ccad706c98f9f
Poovamraj T T <poovamraj@gmail.com>
2018-10-06 17:59:42 +0530

    redundant member baseTheme introduced early in development is removed

--- facecapture/src/main/java/com/poovam/facecapture/FaceCapturer.kt
+++ facecapture/src/main/java/com/poovam/facecapture/FaceCapturer.kt
@@ -10,27 +10,12 @@ import com.poovam.facecapture.rootactivity.view.RootActivity
  * Created by poovam-5255 on 9/28/2018.
  * The class that will act as entry point
  */
-
-//:Deviate: We wont be using builder pattern here since Kotlin supports named parameters and it is much neater
-class FaceCapturer (
-        private val baseTheme: BaseTheme = BaseTheme.DARK
-) {
+class FaceCapturer () {
 
     private val persistence = InMemoryPersistence
 
-    enum class BaseTheme{
-        LIGHT,DARK
-    }
-
-    companion object {
-        val BASE_THEME = "BASE_THEME"
-    }
-
     fun startActivityForResult(activity: AppCompatActivity, requestCode: Int){
         val intent = Intent(activity.applicationContext, RootActivity::class.java)
-        intent.run {
-            putExtra(BASE_THEME,baseTheme.name)
-        }
         activity.lifecycle.addObserver(InMemoryPersistence)
         activity.startActivityForResult(intent, requestCode)
     }
